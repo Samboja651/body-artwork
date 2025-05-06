a74410d25e22dc3eff09b155253eb24a1743e59c
Samboja651 <waribekihiko651@gmail.com>
2025-05-06 18:14:50 +0300

    about page

--- app.py
+++ app.py
@@ -9,3 +9,13 @@ app = Flask(__name__)
 def home():
     """home page"""
     return render_template("home.html")
+
+@app.get("/booking")
+def booking():
+    """book a session with art experience"""
+    return render_template("booking.html")
+
+@app.get('/about')
+def about():
+    """know more about art experience"""
+    return render_template("about.html")

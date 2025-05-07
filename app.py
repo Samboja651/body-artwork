eb98754d42133dca40ec4cf1a6c1a9ab21aecca5
Samboja651 <waribekihiko651@gmail.com>
2025-05-07 10:35:25 +0300

    payment redirect after schedulilng

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 """art-experience"""
+import os
 from flask import Flask, render_template
+from dotenv import load_dotenv
 
+load_dotenv('.env')
+
+PAYMENT_LINK = os.getenv('PAYMENT_LINK')
+BOOKING_LINK = os.getenv('BOOKING_LINK')
 
 app = Flask(__name__)
 
@@ -13,7 +19,7 @@ def home():
 @app.get("/booking")
 def booking():
     """book a session with art experience"""
-    return render_template("booking.html")
+    return render_template("booking.html", booking_link=BOOKING_LINK)
 
 @app.get('/about-us')
 def about():

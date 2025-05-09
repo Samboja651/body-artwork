bce957fd6a7f976a9a4de77bcdedccc6aa375153
Samboja651 <waribekihiko651@gmail.com>
2025-05-09 12:11:36 +0300

    setup external database

--- app.py
+++ app.py
@@ -1,102 +1,140 @@
 """art-experience"""
+
 import os
-import sqlite3
+from urllib.parse import urlparse
 import datetime
-import bcrypt
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask import Flask, render_template, request, redirect, flash, url_for
 from dotenv import load_dotenv
-from setup_db import setup_db
+import psycopg2
 
-load_dotenv('.env')
 
-PAYMENT_LINK = os.getenv('PAYMENT_LINK')
-BOOKING_LINK = os.getenv('BOOKING_LINK')
+load_dotenv(".env")
+
+PAYMENT_LINK = os.getenv("PAYMENT_LINK")
+BOOKING_LINK = os.getenv("BOOKING_LINK")
+DATABASE = os.getenv("INTERNAL_DATABASE_URL")
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET")
 app.permanent_session_lifetime = datetime.timedelta(minutes=45)
 
-# setup database
-setup_db()
+
+# --------------------------
+# configure & connect db
+# --------------------------
+def connect_db():
+    """connect database"""
+    result = urlparse(DATABASE)
+    try:
+        connection = psycopg2.connect(
+            host=result.hostname,
+            port=result.port,
+            dbname=result.path[1:],
+            user=result.username,
+            password=result.password,
+        )
+        print("db connected")
+        return connection
+    except (
+        psycopg2.InterfaceError,
+        psycopg2.ProgrammingError,
+        psycopg2.DatabaseError,
+    ) as e:
+        return f"Connection Failed: {e}"
+
 
 @app.get("/")
 def home():
     """home page"""
     return render_template("home.html")
 
+
 @app.get("/booking")
 def booking():
     """book a session with art experience"""
     return render_template("booking.html", booking_link=BOOKING_LINK)
+
 
 # @app.get('/about-us')
 # def about():
 #     """know more about art experience"""
 #     return render_template("about.html")
 
-@app.route('/sign_up', methods=['GET', 'POST'])
+
+@app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
     """Create user account"""
     if request.method == "POST":
-        email = request.form.get('email')
-        first_name = request.form.get('fname')
-        last_name = request.form.get('lname')
-        password = request.form.get('password')
+        email = request.form.get("email")
+        first_name = request.form.get("fname")
+        last_name = request.form.get("lname")
+        password = request.form.get("password")
 
         # Hash the password
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        hashed_password = generate_password_hash(password)
 
         # Save user details to the database
+        conn = connect_db()
+        cursor = conn.cursor()
         try:
-            conn = sqlite3.connect('database.db')
-            cursor = conn.cursor()
-            cursor.execute('''
+            cursor.execute(
+                """
                 INSERT INTO users (email, first_name, last_name, password)
-                VALUES (?, ?, ?, ?)
-            ''', (email, first_name, last_name, hashed_password))
+                VALUES (%s, %s, %s, %s)
+            """,
+                (email, first_name, last_name, hashed_password),
+            )
             conn.commit()
             conn.close()
 
             flash("Account created successfully!", "success")
-            return redirect(url_for('sign_in'))
-        except sqlite3.IntegrityError:
+            return redirect(url_for("sign_in"))
+        except TypeError:
+            conn.close()
             flash("Email already exists. Please use a different email.")
-            return redirect(url_for('sign_up'))
-        except sqlite3.OperationalError as e:
-            flash("Server Downtime. Try again later.")
+            return redirect(url_for("sign_up"))
+        except psycopg2.IntegrityError:
+            conn.close()
+            flash("Email Already exist. Sign in.")
+            return redirect(url_for("sign_up"))
+        except Exception as e:
+            conn.close()
             print(e)
-            return redirect(url_for('sign_up'))
+            flash("Server under maintenance. Try later.")
+            return redirect(url_for("sign_up"))
 
-    return render_template('register.html')
+    return render_template("register.html")
 
-@app.route('/sign_in', methods=['GET', 'POST'])
+
+@app.route("/sign_in", methods=["GET", "POST"])
 def sign_in():
     """Sign in user"""
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
 
+        # Connect to the database
+        conn = connect_db()
+        cursor = conn.cursor()
         try:
-            # Connect to the database
-            conn = sqlite3.connect('database.db')
-            cursor = conn.cursor()
-
             # Fetch the user by email
-            cursor.execute('SELECT password FROM users WHERE email = ?', (email,))
+            cursor.execute("SELECT password FROM users WHERE email = %s", (email,))
             result = cursor.fetchone()
             conn.close()
 
             if result:
                 # Verify the password
                 stored_password = result[0]
-                if bcrypt.checkpw(password.encode('utf-8'), stored_password):
+                if check_password_hash(stored_password, password):
                     flash("Signed in successfully!", "success")
-                    return redirect(url_for('home'))
+                    return redirect(url_for("home"))
                 flash("Invalid email or password.", "danger")
 
-        except sqlite3.OperationalError as e:
-            flash("Server Downtime. Try again later.")
+        except Exception as e:
+            conn.close()
             print(e)
-            return redirect(url_for('sign_in'))
+            flash("Server under maintenance. Try later.")
+            return redirect(url_for("sign_in"))
 
     return render_template("login.html")
